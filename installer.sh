c446787fbf3f7a5659dec92bd36fdbb76d62729f
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-28 21:07:36 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,15 @@
+while [true]
+do
+  sudo mkdir /var/www/temp
+  cd /var/www/temp
+  sudo git clone https://github.com/DNAmaster10/multiplayer_movement_test.git
+  cd ../
+  sudo rm -dr /var/www/html
+  sudo mkdir /var/www/html
+  sudo chown www-data:www-data /var/www/html
+  sudo chmod 770 /var/www/html
+  sudo cp -r /var/www/temp/multiplayer_movement_test /var/www/html
+  sudo rm -r -r /var/www/temp
+  echo -n "Press enter to update site"
+  read result
+done
