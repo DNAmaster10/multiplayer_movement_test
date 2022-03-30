d38fb1a957d2b3fa0b88b99b5201760a00a5d63f
DNAmaster10 <louishollobon@outlook.com>
2022-03-30 19:41:54 +0100

    update

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -1,3 +1,14 @@
+var c = document.getElementById("mian_game_canvas");
+var ctx = c.getContext("2d);
+$.ajax({
+        url: './send_message_handle.php',
+        type: "POST",
+        data: {message:input_message},
+        success: function(data) {
+            console.log(data);
+            slideIn();
+        }
+});
 function draw() {
 
 }
