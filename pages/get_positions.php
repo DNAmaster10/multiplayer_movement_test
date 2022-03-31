087c397edb51cc66b1c00cfc269a282d884eca7b
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 21:08:50 +0100

    Update get_positions.php

--- pages/get_positions.php
+++ pages/get_positions.php
@@ -3,7 +3,7 @@
     $file_path = $_SERVER["DOCUMENT_ROOT"];
     include "$file_path/php/dbh.php";
     $time = time();
-    $new_time = time - 100;
+    $new_time = $time - 100;
     $username = $_SESSION["username"];
     $sql = "SELECT player_x,player_y FROM player_positions WHERE player!='$username' AND last_update>$new_time";
     $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
