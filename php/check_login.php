afff5472fdb47a2531175cc58771cfdecdb9ea85
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 19:19:18 +0100

    Update check_login.php

--- php/check_login.php
+++ php/check_login.php
@@ -2,7 +2,7 @@
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
-    if (!isset $file_path) {
+    if (!isset($file_path)) {
         $file_path = $_SERVER["DOCUMMENT_ROOT"];
     }
     $table_name = "users";
