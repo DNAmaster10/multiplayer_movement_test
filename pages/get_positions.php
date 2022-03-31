8e172481c99cc0b1ccd8701a95d2ec37dac5f80d
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 21:35:24 +0100

    Update get_positions.php

--- pages/get_positions.php
+++ pages/get_positions.php
@@ -2,17 +2,14 @@
     session_start();
     $file_path = $_SERVER["DOCUMENT_ROOT"];
     include "$file_path/php/dbh.php";
-    if ($conn->connect_error) {
-    die("La connexion a échoué : " . $conn->connect_error);
-}
     $time = time();
-    $new_time = $time - 10000;
+    $new_time = time - 100;
     $username = $_SESSION["username"];
-    $sql = "SELECT player_x, player_y FROM player_positions;";
-    $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
-    if ($raw_resut -> num_rows > 0) {
+    $sql = "SELECT player_x,player_y FROM player_positions WHERE player!='$username' AND last_update>''$new_time'";
+    $raw_result = mysqli_query($conn, $sql);
+    if ($raw_resut -> num_rows > 0 {
         while ($row = mysqli_fetch_array($raw_result)) {
-            echo ($row["player_x"].",".$row["player_y"].".");
+            echo ("$row["player_x"],$row["player_y"].");
         }
     }
 ?>
