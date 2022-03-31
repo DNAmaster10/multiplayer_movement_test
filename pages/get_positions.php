880e4f5272219ee59ef55b3bdc1374c3458a4c48
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 21:39:29 +0100

    Update get_positions.php

--- pages/get_positions.php
+++ pages/get_positions.php
@@ -5,8 +5,8 @@
     $time = time();
     $new_time = $time - 100;
     $username = $_SESSION["username"];
-    $sql = "SELECT player_x,player_y FROM player_positions WHERE player!='$username'";
-    $raw_result = mysqli_query($conn, $sql);
+    $sql = "SELECT player_x,player_y FROM player_positions WHERE player!='$username' AND last_update>$new_time";
+    $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
     if ($raw_result -> num_rows > 0) {
         while ($row = mysqli_fetch_array($raw_result)) {
             echo ($row["player_x"].",".$row["player_y"].".");
