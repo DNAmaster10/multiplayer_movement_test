95b6693cb51ea5c150666297c112982b910e5ec5
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 21:35:59 +0100

    Update get_positions.php

--- pages/get_positions.php
+++ pages/get_positions.php
@@ -9,7 +9,7 @@
     $raw_result = mysqli_query($conn, $sql);
     if ($raw_resut -> num_rows > 0 {
         while ($row = mysqli_fetch_array($raw_result)) {
-            echo ("$row["player_x"],$row["player_y"].");
+            echo ($row["player_x"].",".$row["player_y"].".");
         }
     }
 ?>
