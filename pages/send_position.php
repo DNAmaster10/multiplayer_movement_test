c030f1485c4fcc98d3adb498d0c5e115b3e04628
DNAmaster10 <louishollobon@outlook.com>
2022-03-31 22:10:32 +0100

    Update send_position.php

--- pages/send_position.php
+++ pages/send_position.php
@@ -5,5 +5,5 @@
     $player_x = $conn -> real_escape_string($_GET["player_x"]);
     $time = time();
     $sql = "UPDATE player_positions SET player_x='$player_x',player_y='$player_y',last_update='$time' WHERE player='".$_SESSION["username"].";";
-    mysqli_query ($conn, $sql);
+    mysqli_query ($conn, $sql) or die (mysqli_error($conn));
 ?>
