6ad03bcd8039f2cbcf2b21605302464e7c05ae5d
DNAmaster10 <louishollobon@outlook.com>
2022-04-01 22:06:08 +0100

    Update main.js

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -67,7 +67,7 @@ function get_positions() {
     });
 }
 function interpolate_calculate() {
-    for (let i = 0; i < total_positions; i++) {
+    for (let i = 0; i < total_positions -1; i++) {
         temp_array = old_positions_array[i].split(",");
         temp_array_2 = positions_array[i].split(",");
         dx = (temp_array_2[0] - temp_array[0]);
