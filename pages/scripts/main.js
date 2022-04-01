d4496591387d299b69bd74dabd50f4763b346abe
DNAmaster10 <louishollobon@outlook.com>
2022-04-01 20:48:01 +0100

    e

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -88,7 +88,7 @@ function move_others() {
         ctx.lineWidth = "4";
         ctx.rect(new_x,new_y,10,10);
         ctx.stroke();
-        current_interpolat_positions_array[i] = temp;
+        current_interpolate_positions_array[i] = temp;
     }
 }
 function main_game_loop() {
