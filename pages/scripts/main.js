241cd193dc9b49c4909113d9d3d82fd0ccdcacdb
DNAmaster10 <louishollobon@outlook.com>
2022-04-04 03:21:13 +0100

    Update main.js

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -30,20 +30,69 @@ function update_player() {
     ctx.lineWidth = "4";
     ctx.rect(player_x, player_y, 10, 10);
     ctx.stroke();
-    send_player_position();
 }
 function get_other_positions() {
     $.ajax({
         url: './get_positions.php',
         type: "GET",
         success: function(data) {
+            var old_other_player_positions_array = other_player_positions_array;
             var other_player_positions_array = data.split(".");
         }
     });
 }
+function calculate_vectors() {
+    for (let i = 0; i < other_players_ammount; i++) {
+        var current_player_position = other_player_positions_array[i].split(",");
+        var old_current_player_position = old_other_player_positions_array.split(",");
+        var old_current_player_x = old_current_player_position[0];
+        var old_current_player_y = old_current_player_position[1];
+        var current_player_x = current_player_position[0];
+        var current_player_y = current_player_position[1];
+        var current_player_x_change = current_player_x - old_current_player_x;
+        var current_player_y_change = current_player_y - old_current_player_y;
+        var vector_string = "";
+        if (current_player_x_change > 0) {
+            vector_x = current_player_x_change / get_request_delay;
+            vector_string = vector_x + ",";
+        }
+        else {
+            vector_string = "0,";
+        }
+        if (current_player_y_change > 0) {
+            vector_y = current_player_y_change / get_request_delay;
+            vector_string = vector_string + vector_y;
+        }
+        else {
+            vector_string = vector_string + "0";
+        }
+        var other_player_vectors[i] = vector_string;
+    }
+}
+function move_others();
+    for (let i = 0; i < other_players_ammount; i++) {
+        var current_player_vector = other_player_vectors[i].split(",");
+        var current_player_position = other_player_positions_array[i].split(",");
+        var current_player_position_new_x = current_player_position[0] + current_player_vector[0];
+        var current_player_position_new_y = current_player_position[1] + current_player_bector[1];
+        var new_player_position = current_player_position_new_x + "," + current_player_position_new_y;
+        other_player_positions_array[i] = new_player_position;
+        ctx.beginPath();
+        ctx.lineWidth = "4";
+        ctx.rect(current_player_position_new_x, current_player_position_new_y, 10, 10);
+        ctx.stroke();
+    }
 function update_others() {
-    get_other_positions();
-
+    loop_count = loop_count + 1;
+    if (get_request_delay == loop_count) {
+        send_player_position();
+        get_other_positions();
+        calculate_vectors();
+    }
+    var other_players_ammount = other_player_possitions_array.length;
+    if (other_players_ammount > 0) {
+        move_others();
+    }
 }
 function interpolate_calculate() {
 }
